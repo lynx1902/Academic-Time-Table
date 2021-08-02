d9ff461c7aaebae3695838a6cc901ad8afe1a81e
EEEVNIT <87592447+EEEVNIT@users.noreply.github.com>
2021-08-02 18:13:59 +0530

    Update subjects.js

--- EEE_Sem3/subjects.js
+++ EEE_Sem3/subjects.js
@@ -13,32 +13,47 @@ const SUBJECTS = {
 
   "slotC": {
     name: "ITPDE",
-    link: ""
+    link: "https://vnit.webex.com/vnit/j.php?MTID=m9ecf96047e5d66096ab3898a5d54d198"
   },
 
   "slotD": {
     name: "M & I",
-    link: ""
+    link: "https://vnit.webex.com/meet/mrramteke"
   },
 
-  "slotE": {
+  "slotEmon": {
     name: "EMF",
-    link: ""
+    link: "https://vnit.webex.com/vnit/j.php?MTID=m24145695492fef5b0f4b762f6eaef539 "
   },
 
+  "slotEtue": {
+    name: "EMF",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=mc50e1118aae58c9da8f4aa0d82497618 "
+  },
+
+  "slotEwed": {
+    name: "EMF",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=m38961c62fd114d8302dfb8aab05998c1"
+  },
+
+  "slotEfri": {
+    name: "EMF",
+    link: "https://vnit.webex.com/vnit/j.php?MTID=m54ec48889fcac0a4761e32d6113091fd  "
+  },
+ 
   "slotF": {
     name: "S & S",
-    link: ""
+    link: "https://meet.google.com/ykz-jrgz-jiu"
   },
 
   "slotG": {
     name: "EDC",
-    link: ""
+    link: "https://vnit.webex.com/meet/himanshupadole.aapp"
   },
 
   "slotH": {
     name: "NT",
-    link: ""
+    link: "https://vnit.webex.com/meet/hmsuryawanshi"
   }
 };
 
